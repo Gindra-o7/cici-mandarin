c3a0994cd0fb55f042b2fff58751dab9b4493777
Gindra-o7 <gilangindra734@gmail.com>
2025-10-20 20:02:14 +0700

    refactor img join assets

--- popup.js
+++ popup.js
@@ -5,32 +5,32 @@ const closeBtn = document.querySelector("#popup1 .close");
 let popupStep = 1;
 
 document.querySelector(".heroHome-img").addEventListener("click", () => {
-    popup.style.display = "flex"; 
-    popupStep = 1;
-    popupImg.src = "/asset/popup1.png";
+  popup.style.display = "flex";
+  popupStep = 1;
+  popupImg.src = "asset/popup1.png";
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    popup.style.display = "flex"; 
-    popupStep = 1;
-    popupImg.src = "/asset/popup1.png";
+  popup.style.display = "flex";
+  popupStep = 1;
+  popupImg.src = "asset/popup1.png";
 });
 
 popupImg.addEventListener("click", () => {
-    if (popupStep === 1) {
-        popupStep = 2;
-        popupImg.src = "/asset/popup2.png";
-    } else {
-        popup.style.display = "none";
-    }
+  if (popupStep === 1) {
+    popupStep = 2;
+    popupImg.src = "asset/popup2.png";
+  } else {
+    popup.style.display = "none";
+  }
 });
 
 closeBtn.addEventListener("click", () => {
-    popup.style.display = "none";
+  popup.style.display = "none";
 });
 
 popup.addEventListener("click", (e) => {
-    if (e.target === popup) {
-        popup.style.display = "none";
-    }
+  if (e.target === popup) {
+    popup.style.display = "none";
+  }
 });
